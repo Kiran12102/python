bdfd56a1e1e9f9d40f1324dead888f19c1104bda
Kiran S <94137587+Kiran12102@users.noreply.github.com>
2021-11-29 12:24:03 +0530

    to find average of numbers
    finding average of numbers by importing file containing data.

---
+++ file handling avg.py
@@ -0,0 +1,14 @@
+file_in=open('C:\\Users\\kiran\\Desktop\\practice\\temperature.txt')
+file_out=open('C:\\Users\\kiran\\Desktop\\practice\\temperature1.txt', 'w')
+
+
+data=file_in.read()
+while data:
+    sum(data:float)->float
+    num=len(data)
+
+avg=total/num
+
+print(avg)
+
+
